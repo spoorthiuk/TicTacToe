223125031f0f983c6f1d01b47a32f54a72f9196b
user-n2s <nidhish.pratyush@gmail.com>
2024-10-29 21:12:31 -0700

    Updating UI elements

---
+++ app/src/main/java/com/example/tictactoe/LoginActivity.kt
@@ -0,0 +1,36 @@
+package com.example.tictactoe
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var button2 = findViewById<Button>(R.id.button2)
+        // ai button navigation
+        var button1 = findViewById<Button>(R.id.button)
+
+        button1.setOnClickListener {
+            val intent1 = Intent (this, MainActivity::class.java)
+            startActivity(intent1)
+        }
+
+        // multiplayer button navigation
+        button2.setOnClickListener {
+            val intent1 = Intent (this, MultiplayerActivity::class.java)
+            startActivity(intent1)
+        }
+    }
+}
